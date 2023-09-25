89fa80eb22c4afc6ff9ecd355463ecdcc6e49a04
lucabarata <luca.baruselli@gmail.com>
2023-09-25 12:30:51 -0300

    cria header no feed
    adiciona imagens
    cria link perfil

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import { NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Login } from './componentes/Login';
 import { Cadastro } from './componentes/Cadastro';
 import { RecuperacaoSenha } from './componentes/RecuperacaoSenha';
@@ -12,19 +12,19 @@ import { Perfil } from './componentes/Perfil';
 
 const Stack = createNativeStackNavigator();
 
-export default function App(){
- 
+export default function App() {
+
   return (
     // estrutura do app
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Perfil'>
-        <Stack.Screen name='Login' component={Login}/>
-        <Stack.Screen name='Cadastro' component={Cadastro}/>
-        <Stack.Screen name='Passeio' component={Passeio}/>
-        <Stack.Screen name='RecuperacaoSenha' component={RecuperacaoSenha}/>
-        <Stack.Screen name='Feed' component={Feed}/>
-        <Stack.Screen name='AdicionarPet' component={AdicionarPet}/>
-        <Stack.Screen name='Perfil' component={Perfil}/>
+      <Stack.Navigator initialRouteName='Feed'>
+        <Stack.Screen name='Login' component={Login} />
+        <Stack.Screen name='Cadastro' component={Cadastro} />
+        <Stack.Screen name='Passeio' component={Passeio} />
+        <Stack.Screen name='RecuperacaoSenha' component={RecuperacaoSenha} />
+        <Stack.Screen name='Feed' component={Feed} />
+        <Stack.Screen name='AdicionarPet' component={AdicionarPet} />
+        <Stack.Screen name='Perfil' component={Perfil} />
       </Stack.Navigator>
     </NavigationContainer>
   );
