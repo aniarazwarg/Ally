359c7cfb590a5c866b6383985de09b0f4e79d4e0
lucabarata <luca.baruselli@gmail.com>
2023-10-03 17:00:52 -0300

    sobe drawer
    atualiza imports

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import 'react-native-gesture-handler';
 import { Login } from './componentes/Login';
 import { Cadastro } from './componentes/Cadastro';
 import { RecuperacaoSenha } from './componentes/RecuperacaoSenha';
@@ -12,18 +14,30 @@ import { Calendario } from './componentes/Calendario';
 
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
+  return (
+    <Drawer.Navigator screenOptions={{
+      headerShown: false
+    }
+    }>
+      <Drawer.Screen name="Login" component={Login} />
+      <Drawer.Screen name="Cadastro" component={Cadastro} />
+    </Drawer.Navigator>
+  )
+}
 
 export default function App() {
 
   return (
     // estrutura do app
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Feed'>
-        <Stack.Screen name='Login' component={Login} />
-        <Stack.Screen name='Cadastro' component={Cadastro} />
+      <Stack.Navigator initialRouteName='Alo'>
         <Stack.Screen name='Passeio' component={Passeio} />
         <Stack.Screen name='RecuperacaoSenha' component={RecuperacaoSenha} />
-        <Stack.Screen name='Feed' component={Feed} />
+        <Stack.Screen name='Login' component={Login} />
+        <Stack.Screen name='Cadastro' component={Cadastro} />
         <Stack.Screen name='AdicionarPet' component={AdicionarPet} />
         <Stack.Screen name='Perfil' component={Perfil} />
         <Stack.Screen name='Calendario' component={Calendario} />
