86b61e60606408afda38c2545341aebf878fbed9
Aniara Zwarg <112500673+aniarazwarg@users.noreply.github.com>
2023-10-04 11:08:55 -0300

    altera dependencias

--- App.js
+++ App.js
@@ -9,29 +9,10 @@ import { Feed } from './componentes/Feed';
 import { Calendario } from './componentes/Calendario';
 
 const Stack = createNativeStackNavigator();
-<<<<<<< HEAD
-=======
-const Drawer = createDrawerNavigator();
-
-function MyDrawer() {
-  return (
-    <Drawer.Navigator screenOptions={{
-      headerShown: false
-    }
-    }>
-      <Drawer.Screen name="Login" component={Login} />
-      <Drawer.Screen name="Cadastro" component={Cadastro} />
-    </Drawer.Navigator>
-  )
-}
-
-export default function App() {
->>>>>>> parent of da7ee92 (atualiza)
 
 export default function App(){
  
   return (
-<<<<<<< HEAD
 
 <NavigationContainer>
       <Stack.Navigator initialRouteName='Login'>
@@ -41,18 +22,6 @@ export default function App(){
         <Stack.Screen name='RecuperacaoSenha' component={RecuperacaoSenha}/>
         <Stack.Screen name='Feed' component={Feed}/>
         <Stack.Screen name='Hotel' component={Calendario}/>
-=======
-    // estrutura do app
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Alo'>
-        <Stack.Screen name='Passeio' component={Passeio} />
-        <Stack.Screen name='RecuperacaoSenha' component={RecuperacaoSenha} />
-        <Stack.Screen name='Login' component={Login} />
-        <Stack.Screen name='Cadastro' component={Cadastro} />
-        <Stack.Screen name='AdicionarPet' component={AdicionarPet} />
-        <Stack.Screen name='Perfil' component={Perfil} />
-        <Stack.Screen name='Calendario' component={Calendario} />
->>>>>>> parent of da7ee92 (atualiza)
       </Stack.Navigator>
     </NavigationContainer>
   );
