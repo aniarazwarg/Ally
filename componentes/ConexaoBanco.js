03444c918c97d83d4bee0525b54baf0474b07c32
lucabarata <83922338+lucabarata@users.noreply.github.com>
2023-10-10 22:31:43 -0300

    create, read, delete exemplo
    pasta api com index.php e banco de dados exemplo no teams

--- componentes/ConexaoBanco.js
+++ componentes/ConexaoBanco.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, View, Image, TouchableOpacity, Text, TextInput, Button } from "react-native";
+import { StyleSheet, View, Image, TouchableOpacity, Text, TextInput, Button, Touchable } from "react-native";
 
 export function ConexaoBanco({ navigation }) {
     const [produtos, setProdutos] = useState([]);
     const [valor1, setValor1] = useState('');
-    const [valor2, setValor2] = useState('');
 
     const handleInsertData = () => {
         fetch('http://localhost/api/inserir', {
@@ -14,7 +13,6 @@ export function ConexaoBanco({ navigation }) {
             },
             body: JSON.stringify({
                 valor1: valor1,
-                valor2: valor2,
             }),
         })
             .then((response) => response.json())
@@ -26,34 +24,65 @@ export function ConexaoBanco({ navigation }) {
             });
     };
 
+    const deleteRecord = async (id) => {
+        try {
+            const response = await fetch(`http://localhost/api/deletar/${id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-type': 'application/json',
+                }
+            });
+
+            if (response.status === 200) {
+                console.log('Registro excluído com sucesso.');
+            } else {
+                console.error('Erro ao excluir registro.');
+            }
+        } catch (error) {
+            console.error('Erro de rede: ', error);
+        }
+    };
+
+
     function data() {
         fetch('http://localhost/api/produtos')
             .then((response) => response.json())
             .then((json) => setProdutos(json))
     }
 
+    data();
+
     useEffect(() => {
-        data();
     }, []);
 
     return (
         <View>
             <View>
-                {produtos.map((produto) => (
-                    <Text key={produto.ID}>{produto.NOME}</Text>
-                ))}
+                <View>
+                    {produtos.map((produto) => (
+                        <View style={{flexDirection: 'row', justifyContent: 'space-around', marginBottom: 5, alignItems: 'center', marginTop: 20}} key={produto.ID}>
+                            <TextInput
+                            value={produto.NOME} />
+                            <TouchableOpacity onPress={() =>deleteRecord(produto.ID)} style={{backgroundColor: 'blue', borderRadius: 20, padding: 10}}>
+                                <Text style={{color:'white'}}>Deletar</Text>
+                            </TouchableOpacity>
+                        </View>
+                    ))}
+                </View>
+                <TextInput
+                    placeholder="Digite o valor 1"
+                    value={valor1}
+                    onChangeText={(text) => setValor1(text)}
+                />
+                <Button title="Inserir Dados" onPress={handleInsertData} />
             </View>
-            <TextInput
-                placeholder="Digite o valor 1"
-                value={valor1}
-                onChangeText={(text) => setValor1(text)}
-            />
-            <TextInput
-                placeholder="Digite o valor 2"
-                value={valor2}
-                onChangeText={(text) => setValor2(text)}
-            />
-            <Button title="Inserir Dados" onPress={handleInsertData} />
+            {/* <View>
+                <View>
+                    {produtos.map((produto) => {
+                        <Button key={produto.ID} onPress={deleteRecord(produto.ID)}>{produto.ID}</Button>
+                    })}
+                </View>
+            </View> */}
         </View>
     )
 }
