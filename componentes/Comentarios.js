c874f2779c8aa1be0b6a75ea46141555a8897200
Eduardo Elias <142364436+Eduardo-112@users.noreply.github.com>
2023-10-25 20:51:33 -0300

    Update Comentarios.js

--- componentes/Comentarios.js
+++ componentes/Comentarios.js
@@ -1,19 +1,13 @@
-
-
 import react, { useState, useEffect } from "react";
-import { View, StyleSheet, TextInput, TouchableOpacity, Image, ScrollView, StatusBar } from "react-native";
-import { Text } from 'react-native-paper';
-
-
-
-
-
+import { StyleSheet, View, Image, TouchableOpacity, Text, ScrollView, StatusBar } from "react-native";
 
 export function Comentarios({ navigation }) {
 
-
-
     const [comentarios, setComentarios] = useState([]);
+    
+    const [curtidas, setCurtidas] = useState(null);
+    const [descurtidas, setDescurtidas] = useState(null);
+    const [id, setId] = useState(null);
 
     function dataComentarios() {
         fetch('http://localhost/api/comentarios')
@@ -21,45 +15,58 @@ export function Comentarios({ navigation }) {
             .then((json) => setComentarios(json))
     }
 
+    const updateCurtidas = async (id, newData) => {
+        try {
+            const response = await fetch(`http://localhost/api/curtidas/${id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(newData), // Os dados de atualização
+            });
+
+            if (response.status === 200) {
+                console.log('Registro atualizado com sucesso.');
+                console.log(newData)
+            } else {
+                console.error('Erro ao atualizar registro.');
+            }
+        } catch (error) {
+            console.error('Erro de rede:', error);
+        }
+    };
+
+    dataComentarios();
+
     useEffect(() => {
-        dataComentarios();
-    }, []);
+        if (id !== null) {
+            updateCurtidas(id, { curtidas, descurtidas });
+        }
+    }, [curtidas, descurtidas, id]);
 
-
-  return (
-
-    
-
-
-      <View style={styles.container}>
-        <ScrollView style={styles.scrollView}
-          stickyHeaderIndices={[0]}
-          stickyHeaderHiddenOnScroll>
-
-          {/* Header */}
-          <View style={styles.header}>
-             <TouchableOpacity  onPress={() => navigation.navigate('Home')}  style={{width: 40, height: 40}}>
-                  <Image source={require('../assets/voltar.png')}
-                    style={{width: 40, height: 40}}
-                  />
-                </TouchableOpacity>
-
-                <Text style={styles.headerText}>
-                                    
-                                    Comentários</Text>
-                            </View>
-            <View style={styles.headerConteudo}>
-           
-               
-                
-          </View>
-
-          {/* Resto */}
-                         <View style={styles.comentarios}>
+    return (
+        <View style={styles.container}>
+            <ScrollView stickyHeaderIndices={[0]}
+                stickyHeaderHiddenOnScroll>
+                <View style={styles.header}>
+                    <View style={styles.headerConteudo}>
+                        <View>
+                            <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+                                <Image source={require('../assets/voltar.png')}
+                                    style={styles.logosHeader}
+                                />
+                            </TouchableOpacity>
+                        </View>
+                        <View>
+                            <Text style={styles.headerText}>Comentários</Text>
+                        </View>
+                    </View>
+                </View>
+                <View style={styles.comentarios}>
                     {comentarios.map((comentario) => (
-                         <View key={comentario.id}>
+                        <View key={comentario.id}>
                             <View style={styles.comentarioCard}>
-                                 <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', }}>
+                                <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', }}>
                                     <View style={{ flexDirection: 'row', alignItems: 'center' }}>
                                         <Image
                                             source={require('../assets/icon_usuario.png')}
@@ -70,81 +77,81 @@ export function Comentarios({ navigation }) {
                                 </View>
                                 <View style={{ alignItems: 'center', }}>
                                     <Text style={styles.textoComentario}>{comentario.comentario}</Text>
-                                 </View>
-                                 <View style={{ flexDirection: 'row', justifyContent: 'space-evenly' }}>
-                                     <TouchableOpacity>
-                                         <Image
-                                             source={require('../assets/like.png')}
-                                             style={{ width: 20, height: 20 }} />
-                                     </TouchableOpacity>
-                                     <TouchableOpacity>
-                                         <Image
-                                             source={require('../assets/dislike.png')}
-                                             style={{ width: 20, height: 20 }} />
-                                     </TouchableOpacity>
-                                 </View>
-                             </View>
-                         </View>
-                     ))}
-                 </View>
-
-
-        </ScrollView>
-      </View>
-
-
-
-
-
-  );
-};
+                                </View>
+                                <View style={{ flexDirection: 'row', justifyContent: 'space-evenly' }}>
+                                    <View style={{ flexDirection: 'row', justifyContent: 'space-evenly' }}>
+                                        <TouchableOpacity onPress={() => { setId(comentario.id); setCurtidas(comentario.curtidas + 1); setDescurtidas(comentario.descurtidas) }}>
+                                            <Image
+                                                source={require('../assets/like.png')}
+                                                style={{ width: 20, height: 20, marginRight: 10 }} />
+                                        </TouchableOpacity>
+                                        <Text>{comentario.curtidas}</Text>
+                                    </View>
+                                    <View style={{ flexDirection: 'row', justifyContent: 'space-evenly' }}>
+                                        <TouchableOpacity onPress={() => { setId(comentario.id); setDescurtidas(comentario.descurtidas + 1); setCurtidas(comentario.curtidas) }}>
+                                            <Image
+                                                source={require('../assets/dislike.png')}
+                                                style={{ width: 20, height: 20, marginRight: 10 }} />
+                                        </TouchableOpacity>
+                                        <Text>{comentario.descurtidas}</Text>
+                                    </View>
+                                </View>
+                            </View>
+                        </View>
+                    ))}
+                </View>
+            </ScrollView >
+        </View>
+    );
+}
 
 const styles = StyleSheet.create({
-        container: {
-            flex: 1,         
-            backgroundColor: '#F2EAD0',
-        },
-        header: {
-            zIndex: 1,
-            backgroundColor: '#F2EAD0',
-            
-        },
-        headerConteudo: {       
-            alignContent:"center",       
-            width: '100%',
-        },
-       
-        header: { 
-            flexDirection:'row',
-            marginBottom: 25,
-            backgroundColor: '#F2EAD0',
-        },
-        headerText: {        
-            fontSize: 30, 
-            alignSelf:'center',        
-        },
-        comentarioCard: { 
-            alignSelf: 'center',
-            backgroundColor: '#FCF6D7',
-            width: "70%",
-            padding: 10,
-            borderRadius: 10,
-            borderWidth: 1,
-            borderColor: '#C0C0C0',
-            height: 180,
-            justifyContent: 'space-between',
-            marginBottom: 20
-        },
-        nomeUsuario: {
-            marginLeft: 10,
-        },
-        textoComentario: {
-            fontSize: 13,
-            textAlign: 'justify',
-            width: "100%"
-        },
+    container: {
+        flex: 1,
+        backgroundColor: '#F2EAD0',
+        paddingTop: StatusBar.currentHeight,
+    },
+    header: {
+        zIndex: 1,
+        backgroundColor: '#F2EAD0',
+        width: '100%',
+        padding: 10
+    },
+    headerConteudo: {
+        flexDirection: 'row',
+        marginHorizontal: 20,
+        paddingVertical: 5,
+        alignItems: "center",
+    },
+    logosHeader: {
+        width: 30,
+        height: 30,
+        marginRight: 20
+    },
+    headerText: {
+        fontSize: 25,
+    },
+    comentarioCard: {
+        backgroundColor: '#FCF6D7',
+        width: 300,
+        padding: 10,
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: '#C0C0C0',
+        height: 180,
+        justifyContent: 'space-between',
+        marginBottom: 20
+    },
+    nomeUsuario: {
+        marginLeft: 10,
+    },
+    textoComentario: {
+        fontSize: 13,
+        textAlign: 'justify',
+        width: "100%"
+    },
+    comentarios: {
+        alignItems: 'center'
+    }
 
-        comentarios: {
-    
-        }
-    })
+});
