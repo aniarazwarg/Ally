d5789b4dc8f5c03b12a78049117721e8498dc713
lucabarata <luca.baruselli@gmail.com>
2023-11-23 18:45:21 -0300

    atualiza api e banco

--- banco de dados/ally_db.sql
+++ banco de dados/ally_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 06-Nov-2023 às 20:52
+-- Tempo de geração: 23-Nov-2023 às 22:42
 -- Versão do servidor: 10.4.27-MariaDB
 -- versão do PHP: 8.2.0
 
@@ -34,8 +34,29 @@ CREATE TABLE `tb_cao` (
   `id_sexo` char(1) DEFAULT NULL,
   `ds_pelagem` varchar(45) DEFAULT NULL,
   `cd_cliente` bigint(20) DEFAULT NULL,
-  `cd_raca` int(11) DEFAULT NULL
+  `nm_raca` varchar(100) DEFAULT NULL,
+  `ds_porte` varchar(100) NOT NULL,
+  `ds_peso` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Extraindo dados da tabela `tb_cao`
+--
+
+INSERT INTO `tb_cao` (`cd_cao`, `ds_idade`, `nm_cao`, `id_sexo`, `ds_pelagem`, `cd_cliente`, `nm_raca`, `ds_porte`, `ds_peso`) VALUES
+(1, NULL, '', NULL, '', NULL, '', '', ''),
+(2, NULL, '', NULL, '', NULL, '', '', ''),
+(3, NULL, 'Caramelo', NULL, 'Caramelo', NULL, 'Lhasa Apso', 'Pequeno', '5'),
+(4, NULL, 'aaaa', NULL, 'dddd', NULL, 'bbbb', 'cccc', 'eeee'),
+(5, NULL, '', NULL, '', NULL, '', '', ''),
+(6, NULL, '', NULL, '', NULL, '', '', ''),
+(7, NULL, '', NULL, '', NULL, '', '', ''),
+(8, NULL, '', NULL, '', NULL, '', '', ''),
+(9, NULL, '', NULL, '', NULL, '', '', ''),
+(10, NULL, '', NULL, '', NULL, '', '', ''),
+(11, NULL, '', NULL, '', NULL, '', '', ''),
+(12, NULL, '', NULL, '', NULL, '', '', ''),
+(13, NULL, '', NULL, '', NULL, '', '', '');
 
 -- --------------------------------------------------------
 
@@ -49,18 +70,18 @@ CREATE TABLE `tb_cliente` (
   `dt_nasc_cliente` date DEFAULT NULL,
   `email` varchar(30) NOT NULL,
   `senha` varchar(30) NOT NULL,
-  `cpf` varchar(11) NOT NULL
+  `cpf` varchar(14) NOT NULL,
+  `fotoPerfil` varchar(100) DEFAULT NULL,
+  `telefone` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Extraindo dados da tabela `tb_cliente`
 --
 
-INSERT INTO `tb_cliente` (`cd_cliente`, `nm_cliente`, `dt_nasc_cliente`, `email`, `senha`, `cpf`) VALUES
-(10, 'Luca Barata', '0000-00-00', 'luca_barata@hotmail.com', 'barubaru97', '04864211124'),
-(12, 'João', '1998-02-03', 'teste', 'teste', '0123'),
-(13, 'Lucas', '1999-05-09', 'teste1', 'teste', '1234'),
-(14, 'Mateus', '1995-03-02', 'mat@bol', '123', '04864211123');
+INSERT INTO `tb_cliente` (`cd_cliente`, `nm_cliente`, `dt_nasc_cliente`, `email`, `senha`, `cpf`, `fotoPerfil`, `telefone`) VALUES
+(12, 'João', '1998-02-03', 'teste', 'teste', '0123', 'fotoPerfil12.jpg', 0),
+(35, 'Luca Barata', '1997-02-03', 'luca.baruselli@gmail.com', 'barubaru97', '048.642.111-24', NULL, 13997797442);
 
 -- --------------------------------------------------------
 
@@ -84,10 +105,8 @@ CREATE TABLE `tb_comentarios` (
 
 INSERT INTO `tb_comentarios` (`id`, `nome`, `foto`, `comentario`, `curtidas`, `descurtidas`, `ok`) VALUES
 (1, 'Maria', '', 'Gostei muito dos serviços oferecidos pela Brothers.', 1, 0, 1),
-(2, 'João', '', 'Péssimo serviço da Brothers... passeadores horríveis. Pior de todos é o Yan Gay.', 32, 24, 1),
-(4, 'José', '', 'AAAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', 21, 17, 1),
-(5, 'Carlos', '', 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA', 15, 19, 0),
-(6, 'Ana', '', 'AAAAAAAAAAAAAAAAAAAAaaaaaaaaaaAAAAAAAAAAAAAAAAAAAAaaaaaaaaaaAAAAAAAAAAAAAAAAAAAAaaaaaaaaaaAAAAAAAAAAAAAAAAAAAAaaaaaaaaaaAAAAAAAAAAAAAAAAAAAAaaaaaaaaaa', 15, 17, 0);
+(4, 'José', '', 'Profissionais muito competentes e pontuais! Bom demais!', 21, 17, 1),
+(6, 'Ana', '', 'Ótimo serviço! Adorei os passeadores!', 15, 17, 0);
 
 -- --------------------------------------------------------
 
@@ -133,7 +152,18 @@ CREATE TABLE `tb_servico` (
 --
 
 INSERT INTO `tb_servico` (`cd_servico`, `cd_cliente`, `nm_servico`, `dt_checkin`, `dt_checkout`) VALUES
-(1, '', NULL, '2023-11-02', '2023-11-09');
+(1, '', NULL, '2023-11-02', '2023-11-09'),
+(2, '', NULL, '2023-11-03', '2023-11-08'),
+(3, '', NULL, '2023-11-03', '2023-11-08'),
+(4, '', NULL, '2023-11-13', '2023-11-15'),
+(5, '', NULL, NULL, NULL),
+(6, '', NULL, '2023-11-09', '2023-11-16'),
+(7, '', NULL, '2023-11-02', '2023-11-09'),
+(8, '', NULL, '2023-11-09', '2023-11-16'),
+(9, '', NULL, '2023-11-02', '2023-11-16'),
+(10, '', NULL, '2023-11-02', '2023-11-09'),
+(11, '', NULL, '2023-11-01', '2023-11-08'),
+(12, '', NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -158,8 +188,7 @@ CREATE TABLE `tb_vacinas` (
 -- Índices para tabela `tb_cao`
 --
 ALTER TABLE `tb_cao`
-  ADD PRIMARY KEY (`cd_cao`),
-  ADD KEY `fk_raca` (`cd_raca`);
+  ADD PRIMARY KEY (`cd_cao`);
 
 --
 -- Índices para tabela `tb_cliente`
@@ -196,17 +225,23 @@ ALTER TABLE `tb_servico`
 --
 ALTER TABLE `tb_vacinas`
   ADD PRIMARY KEY (`cd_vacina`),
-  ADD KEY `cd_cao` (`cd_cao`);
+  ADD KEY `tb_vacinas_ibfk_1` (`cd_cao`);
 
 --
 -- AUTO_INCREMENT de tabelas despejadas
 --
 
 --
+-- AUTO_INCREMENT de tabela `tb_cao`
+--
+ALTER TABLE `tb_cao`
+  MODIFY `cd_cao` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
 -- AUTO_INCREMENT de tabela `tb_cliente`
 --
 ALTER TABLE `tb_cliente`
-  MODIFY `cd_cliente` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `cd_cliente` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT de tabela `tb_comentarios`
@@ -218,17 +253,11 @@ ALTER TABLE `tb_comentarios`
 -- AUTO_INCREMENT de tabela `tb_servico`
 --
 ALTER TABLE `tb_servico`
-  MODIFY `cd_servico` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `cd_servico` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Restrições para despejos de tabelas
 --
-
---
--- Limitadores para a tabela `tb_cao`
---
-ALTER TABLE `tb_cao`
-  ADD CONSTRAINT `fk_raca` FOREIGN KEY (`cd_raca`) REFERENCES `tb_raca` (`cd_raca`);
 
 --
 -- Limitadores para a tabela `tb_vacinas`
