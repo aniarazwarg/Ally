0fe9acdb81d7a9940ee70094d7f90580a07d2416
lucabarata <83922338+lucabarata@users.noreply.github.com>
2023-12-04 22:42:43 -0300

    aceita/recusa

--- componentes/Reservas.js
+++ componentes/Reservas.js
@@ -5,6 +5,8 @@ import { ScrollView } from "react-native-gesture-handler";
 export function Reservas({ navigation, route }) {
 
     const [reservas, setReservas] = useState([])
+    const [users, setUsers] = useState([])
+    const [statusReserva, setStatusReserva] = useState('Aprovado')
 
     function getReservas() {
         fetch('http://localhost/api/reservas')
@@ -13,8 +15,40 @@ export function Reservas({ navigation, route }) {
             .then((json) => setReservas(json))
     }
 
+    function getUsers() {
+        fetch('http://localhost/api/usuarios')
+            // fetch('http://192.168.0.11/api/usuarios')
+            .then((response) => response.json())
+            .then((json) => setUsers(json))
+    }
+
+    const updateReserva = async (cd_cliente, statusReserva) => {
+        try {
+            const newData = { statusReserva };
+            const response = await fetch(`http://localhost/api/atualizaReserva/${cd_cliente}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(newData), // Os dados de atualização
+            });
+
+            if (response.status === 200) {
+                console.log('Registro atualizado com sucesso.');
+                console.log(newData)
+            } else {
+                console.error('Erro ao atualizar registro.');
+            }
+        } catch (error) {
+            console.error('Erro de rede:', error);
+        }
+    };
+
+
+    console.log(users)
     useEffect(() => {
         getReservas();
+        getUsers();
     }, [,])
 
     return (
@@ -22,16 +56,16 @@ export function Reservas({ navigation, route }) {
             <View style={styles.container}>
                 {reservas.map((reserva) => (
                     <View key={reserva.cd_servico} style={{ borderRadius: 40, borderWidth: 2, padding: 20, width: '80%', margin: 5 }}>
-                        <View>
-                            <Text style={{ fontSize: 18, fontWeight: 'bold' }}>{reserva.cd_servico}</Text>
-                        </View>
                         <Text> {reserva.dt_checkin} - {reserva.dt_checkout}</Text>
                         <Text>Cliente: {reserva.cd_cliente}</Text>
                         <TouchableOpacity onPress={() => navigation.navigate('Cliente', { cd_cliente: reserva.cd_cliente })} style={styles.botaoVer}>
                             <Text style={{ color: 'white' }}>Ver usuário</Text>
                         </TouchableOpacity>
-                        <TouchableOpacity style={styles.botaoVer}>
-                            <Text style={{ color: 'white' }}>Aceitar</Text>
+                        <TouchableOpacity style={styles.botaoAceitar}>
+                            <Text onPress={() => updateReserva(reserva.cd_cliente, 'Aprovado')} style={{ color: 'white' }}>Aceitar</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.botaoNegar}>
+                            <Text onPress={() => updateReserva(reserva.cd_cliente, 'Reprovado')} style={{ color: 'white' }}>Recusar</Text>
                         </TouchableOpacity>
                     </View>
                 ))}
@@ -49,6 +83,22 @@ const styles = StyleSheet.create({
     },
     botaoVer: {
         backgroundColor: '#6FAA9C',
+        padding: 5,
+        alignItems: 'center',
+        borderRadius: 20,
+        marginRight: 2,
+        marginTop: 10,
+    },
+    botaoAceitar: {
+        backgroundColor: '#6f9baa',
+        padding: 5,
+        alignItems: 'center',
+        borderRadius: 20,
+        marginRight: 2,
+        marginTop: 10,
+    },
+    botaoNegar: {
+        backgroundColor: '#aa6f7d',
         padding: 5,
         alignItems: 'center',
         borderRadius: 20,
